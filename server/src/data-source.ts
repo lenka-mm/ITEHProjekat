3f0c91ae4494c60cce967324a1295246951cab5b
akicacoder <anja99nik@gmail.com>
2022-09-15 21:35:01 +0200

    Added needed libraries

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -1,17 +1,16 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
-import { User } from "./entity/User"
 
 export const AppDataSource = new DataSource({
     type: "mysql",
     host: "localhost",
     port: 3306,
-    username: "test",
-    password: "test",
-    database: "test",
+    username: "root",
+    password: "root",
+    database: "cinema",
     synchronize: true,
     logging: false,
-    entities: [User],
-    migrations: [],
+    entities: ["src/entity/**/*.ts"],
+    migrations: ["src/migration/**/*.ts"],
     subscribers: [],
 })
--- server/src/entity/User.ts
+++ server/src/entity/User.ts
@@ -1,4 +1,5 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import { EncryptionTransformer } from "typeorm-encrypted";
 
 @Entity()
 export class User {
@@ -13,6 +14,16 @@ export class User {
     lastName: string
 
     @Column()
-    age: number
+    email: string;
 
+    @Column({
+        select: false,
+        transformer: new EncryptionTransformer({
+            key: 'e41c966f21f9e157780246fff924e6a3feddd751f201304213b2f845d8841a61',
+            algorithm: 'aes-256-cbc',
+            ivLength: 16,
+            iv: 'ff5ac19190424b1d88f9419ef949ae56'
+        })
+    })
+    password: string;
 }
